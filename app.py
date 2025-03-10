5d916f4a675ed4635a05fe716d846a9fb64680f4
iboet2004 <iboet2004@gmail.com>
2025-03-10 08:24:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ stemmer = factory.create_stemmer()
 
 # ✅ Daftar stopwords bahasa Indonesia yang diperluas
 STOPWORDS = set([
-    "yang", "dan", "di", "dengan", "ke", "dalam", "untuk", "atau", "kami","bagai","hingga","yakin","sendiri","mungkin","dukung","hanya","tempat",
-    "kita", "ini", "itu", "pada", "adalah", "dari", "sebagai", "akan", "juga","pasti", "harap","sendiri","dukung","masuk", "jelas","upaya","tetapi",
-    "telah", "agar", "maupun", "bagi", "tersebut", "dapat", "bahwa", "demi","butuh","langkah","sangat","penting","lanjut",
-    "guna", "melalui", "sehingga", "lebih", "terhadap", "serta", "oleh", "perlu"
+    "yang", "dan", "di", "dengan", "ke", "dalam", "untuk", "atau", "kami",
+    "kita", "ini", "itu", "pada", "adalah", "dari", "sebagai", "akan", "juga",
+    "telah", "agar", "maupun", "bagi", "tersebut", "dapat", "bahwa", "demi",
+    "guna", "melalui", "sehingga", "lebih", "terhadap", "serta", "oleh", "perlu"
 ])
 
 def bersihkan_teks(teks):
